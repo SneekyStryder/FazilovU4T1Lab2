130d19b8cb9d06a55dab60e3c4a56095ce06beca
Imran Fazilov <ifazilov3584@bths.edu>
2022-11-09 12:31:53 -0500

    period end

--- src/Timer.java
+++ src/Timer.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Timer {
+import java.util.Scanner;
+
+public class Timer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What number do you want to count up to? ");
+        int countTo = scanner.nextInt();
+
+        int counter = 1;
+        while (counter < countTo + 1) {
+            System.out.println(counter);
+            counter++;
+        }
+    }
 }
